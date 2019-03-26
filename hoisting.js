0759807c6896f4672046f149c4d10f2ba20bc58e
Tanuka Das <das.tanuka@yahoo.com>
2019-03-26 00:52:57 +0000

    Automatically backed up by Learn

--- hoisting.js
+++ hoisting.js
@@ -4,9 +4,9 @@ function callMe() {
   console.log("and this is crazy..");
   console.log("but here's my number..");
   console.log("so call me");
-  var lyric = "maybe";
-  return lyric;
   //var lyric = "maybe";
+  return lyric;
+  var lyric = "maybe";
 }
 //callMe()
 console.log(callMe());
