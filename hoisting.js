88cb547a136906bd83fd219f34ea521ac2f3258b
Tanuka Das <das.tanuka@yahoo.com>
2019-03-26 01:02:11 +0000

    Automatically backed up by Learn

--- hoisting.js
+++ hoisting.js
@@ -18,7 +18,8 @@ function crazy() {
 
   var thisIsCrazy = function (){
     console.log("hey!!!")
-  }thisIsCrazy();
+  }
+  thisIsCrazy();
 }
 //----------------------------------------------------->
 function sayMyName() {
