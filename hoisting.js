b10dcb0b3a77d9530055b855b646ddc649a24cd7
Tanuka Das <das.tanuka@yahoo.com>
2019-03-26 00:58:38 +0000

    Automatically backed up by Learn

--- hoisting.js
+++ hoisting.js
@@ -1,6 +1,6 @@
 
 function callMe() {
-  + var lyric = "maybe";
++ var lyric = "maybe";
   console.log("I just met you...");
   console.log("and this is crazy..");
   console.log("but here's my number..");
