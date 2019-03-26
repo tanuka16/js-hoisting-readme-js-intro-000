4e02813ff9e0048e6b7f2d9678717244cdbc530c
Tanuka Das <das.tanuka@yahoo.com>
2019-03-26 00:48:32 +0000

    Automatically backed up by Learn

--- hoisting.js
+++ hoisting.js
@@ -1,13 +1,17 @@
+//var lyric = "maybe";
 function callMe() {
   console.log("I just met you...");
   console.log("and this is crazy..");
   console.log("but here's my number..");
   console.log("so call me");
-  return lyric;
   var lyric = "maybe";
+  return lyric;
+  //var lyric = "maybe";
 }
-
-
+callMe()
+//console.log(callMe());
+//----------------------------------------------------
+crazy();
 function crazy() {
   // fix the code in here:
   thisIsCrazy();
@@ -24,7 +28,8 @@ function sayMyName() {
   sayMy();
 
   function sayMy() {
-    console.log(name);
+    //console.log(name);
     var name = "Kristin";
+    console.log(name);
   }
 }
